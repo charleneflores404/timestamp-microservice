67faf51c0b1ac82bb253b405d1c1c06306e7f8ae
Charlene <charlene.flores.404@gmail.com>
2025-07-10 10:39:06 +0800

    handle optional /api/date? route with current date fallback

--- index.js
+++ index.js
@@ -2,31 +2,51 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+// date endpoint
+app.get("/api/:date?", (req, res) => {
+  // '?' at the end of params means it is optional
+  const inputDate = req.params.date;
 
+  const date = !inputDate
+    ? new Date() // current date if no input / empty string / no params date in URL
+    : isNaN(Number(inputDate)) // check if params date is valid
+    ? new Date(inputDate) // ISO date string (e.g. 2025-07-10)
+    : new Date(Number(inputDate)); // Unix timestamp in milliseconds as a string (e.g. 1752105600000)
+
+  console.log(date);
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
